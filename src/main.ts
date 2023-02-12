cbea6485745c7a24b8f2d9383f897be54087fb31
Ruje Alfon <ruje.alfon@bcstechnology.com.au>
2023-02-12 11:28:17 +0800

    Initial commit for v2

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,14 @@
-import { createApp } from "vue";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-import axios from "axios";
-import VueAxios from "vue-axios";
+import App from './App.vue'
+import router from './router'
 
-import api from "@/plugins/api";
-
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import './assets/main.css'
 
 const app = createApp(App)
-  .use(store)
-  .use(router)
-  .use(VueAxios, axios)
-  .use(api);
 
-// // Plugin for validating some data
+app.use(createPinia())
+app.use(router)
 
-// declare module "@vue/runtime-core" {
-//   export interface ComponentCustomProperties {
-//     $validate: (message: string) => void;
-//   }
-// }
-
-// app.config.globalProperties.gg = (message: string) => console.log(message);
-
-// app.config.globalProperties.foo = "bar";
-app.mount("#app");
+app.mount('#app')
--- src/services/api/auth.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default class Auth {
-  login(email: string, password: string) {
-    return axios.post("/auth/login", { email, password });
-  }
-}
