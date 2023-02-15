67a65f33239b0fde100843b7f87c6dded91da225
Ruje Alfon <ruje.alfon@bcstechnology.com.au>
2023-02-15 22:02:07 +0800

    add auto lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,9 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-recommended',
     'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
   ],
